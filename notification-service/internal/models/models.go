fabd4e54c48e0204cb14e6f613f603554df64670
wiwiieie011 <wiwiieie011@gmail.com>
2026-01-13 15:29:45 +0300

    добавил слой репозитория для уведомлений (#5)
    * добавил слой репозитория для уведомлений
    
    * сделал исправления в репозиторий слое и в структуре
    
    * добавил enum переменные
    
    * изменил немного функцию получения уведомлений
    
    * изменил немного структуру

--- notification-service/internal/models/models.go
+++ notification-service/internal/models/models.go
@@ -1,1 +1,25 @@
 package models
+
+import (
+	"gorm.io/gorm"
+)
+
+type Notification struct {
+	gorm.Model
+	UserID    uint `json:"user_id" gorm:"index"`
+	Type      string `json:"type"` // тут либо покупка билетов, уведомление о мероприятиях, и напоминания 
+	Title     string `json:"title"`
+	Body      string `json:"body"`
+	Read      bool `json:"read"`
+}
+
+type NotificationPreference struct {
+	UserID uint `gorm:"primaryKey"`
+
+	TicketPurchased bool // отключает уведомление о покупке билетов 
+	EventCanceled   bool // отклюает уведомления о мероприятиях
+	EventReminder   bool // отключает напоминания 
+
+	PushEnabled  bool   
+	InAppEnabled bool
+}
--- notification-service/internal/repository/notification_repository.go
+++ notification-service/internal/repository/notification_repository.go
@@ -1,1 +1,108 @@
 package repository
+
+import (
+	"log/slog"
+	"notification-service/internal/dto"
+	"notification-service/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type NotificationRepo interface {
+	GetNotifications(userID uint, limit int, lastID uint) ([]models.Notification, error)
+	AllRead(userID uint) error
+	ReadNotificationsByID(userID, id uint) error
+	DeleteNotificationsByID(userID, id uint) error
+	GetNotificationPreferences(userID uint) (*models.NotificationPreference, error)
+	UpdateNotificationPreferences(pref *models.NotificationPreference) error
+	UnreadNotificationsCounts(userID uint) (int64, error)
+}
+
+type notificationRepo struct {
+	db  *gorm.DB
+	log *slog.Logger
+}
+
+func NewNotificationRepo(db *gorm.DB, log *slog.Logger) NotificationRepo {
+	return &notificationRepo{
+		db:  db,
+		log: log,
+	}
+}
+
+func (r *notificationRepo) GetNotifications(userID uint, limit int, lastID uint) ([]models.Notification, error) {
+	var nots []models.Notification
+
+	q := r.db.Select("id, title, body, read, created_at").
+		Where("user_id = ?", userID).
+		Order("id DESC").
+		Limit(limit)
+
+	if lastID > 0 {
+		q = q.Where("id < ?", lastID)
+	}
+
+	if err := q.Find(&nots).Error; err != nil {
+		return []models.Notification{}, nil
+	}
+
+	return nots, nil
+}
+
+func (r *notificationRepo) AllRead(userID uint) error {
+	if err := r.db.
+		Model(&models.Notification{}).
+		Where("user_id = ? AND read = ?", userID, false).
+		Update("read", true).Error; err != nil {
+		return dto.ErrNotificationUpdateFailed
+	}
+
+	return nil
+}
+
+func (r *notificationRepo) ReadNotificationsByID(userID, id uint) error {
+	if err := r.db.
+		Model(&models.Notification{}).
+		Where("id = ? AND user_id = ?", id, userID).
+		Update("read", true).Error; err != nil {
+		return dto.ErrNotificationUpdateFailed
+	}
+
+	return nil
+}
+
+func (r *notificationRepo) DeleteNotificationsByID(userID, id uint) error {
+	if err := r.db.
+		Where("id = ? AND user_id = ?", id, userID).
+		Delete(&models.Notification{}).Error; err != nil {
+		return dto.ErrNotificationDeleteFailed
+	}
+
+	return nil
+}
+
+func (r *notificationRepo) GetNotificationPreferences(userID uint) (*models.NotificationPreference, error) {
+	var pref models.NotificationPreference
+	if err := r.db.
+		Where("user_id = ?", userID).
+		First(&pref).Error; err != nil {
+		return nil, dto.ErrNotificationPreferencesNotFound
+	}
+	return &pref, nil
+}
+
+func (r *notificationRepo) UpdateNotificationPreferences(pref *models.NotificationPreference) error {
+	if err := r.db.Save(pref).Error; err != nil {
+		return dto.ErrNotificationPreferencesUpdateFailed
+	}
+	return nil
+}
+
+func (r *notificationRepo) UnreadNotificationsCounts(userID uint) (int64, error) {
+	var count int64
+	if err := r.db.Model(&models.Notification{}).Where("user_id = ? AND read = ?", userID, false).Count(&count).Error; err != nil {
+		return 0,  dto.ErrUnreadCountFailed
+	}
+	
+	return count, nil
+}
