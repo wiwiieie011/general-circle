b0283e68139d91a344a8717aaf5d7cbbc2271c5c
dzhambazbiev-ux <dzhamb.azbiev@gmail.com>
2026-01-14 11:44:02 +0300

    убрал тег json поля Events в Category

--- event-service/internal/models/category.go
+++ event-service/internal/models/category.go
@@ -5,5 +5,5 @@ import "gorm.io/gorm"
 type Category struct {
 	gorm.Model
 	Name   string  `json:"name" gorm:"not null;uniqueIndex"`
-	Events []Event `json:"events,omitempty" gorm:"foreignKey:CategoryID"`
+	Events []Event `json:"-" gorm:"foreignKey:CategoryID"`
 }
